54ec4c2de0bb09e218cd2761d0a48b3d2e1f2457
Serhat Yilmazturk <seroman@gmail.com>
2016-09-28 15:29:09 +0300

    Initial commit

---
+++ SLDExporter/SLDExporter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SLDExporter
+{
+    public class SLDExporter : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public SLDExporter()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
